407ce8adf0dc92cc5567518d84f81da7118549c2
박규리 <dw05131@naver.com>
2023-11-16 11:16:09 +0900

    app 생성

--- final_pjt_back/final_pjt_back/settings.py
+++ final_pjt_back/final_pjt_back/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'products',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
