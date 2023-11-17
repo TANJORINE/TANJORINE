23fada70f798aad176c937d9adb59d856d3524fa
박규리 <dw05131@naver.com>
2023-11-17 17:50:57 +0900

    지도 수정

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -3,7 +3,7 @@
   <header>
     <nav class="navbar navbar-expand-sm bg-body-tertiary">
       <RouterLink :to="{ name: 'home' }">Home</RouterLink> 
-      <RouterLink :to="{ name: 'exchangerate' }">환율 계산기</RouterLink> 
+      <RouterLink :to="{ name: 'exchangeRate' }">환율 계산기</RouterLink> 
       <RouterLink :to="{ name: 'bankMap' }">은행 찾기</RouterLink>
       <div :class="{ checkLogin : store.isLogin }">
         <RouterLink :to="{ name: 'SignUpView' }" >회원 가입</RouterLink>
